5f9b2ad4136bae79e1d530ea32c24018e8e67aa3
Adam Dąbrowski <190139@edu.p.lodz.pl>
2019-06-13 21:45:13 +0200

    Test if liking a post by unconfirmed user throws DomainError

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,7 +1,13 @@
 package edu.iis.mto.blog.domain;
 
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -18,6 +24,13 @@ import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.BlogDataMapper;
 import edu.iis.mto.blog.services.BlogService;
 
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.junit.Assert.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
@@ -25,19 +38,49 @@ public class BlogManagerTest {
     @MockBean
     UserRepository userRepository;
 
+    @MockBean
+    BlogPostRepository postRepository;
+
+    @MockBean
+    LikePostRepository likeRepository;
+
     @Autowired
     BlogDataMapper dataMapper;
 
     @Autowired
     BlogService blogService;
 
-    @Test
+    private User unconfirmedUser;
+    private BlogPost post;
+
+    @Before
+    public void setUp() {
+        unconfirmedUser = new User();
+        unconfirmedUser.setId(1L);
+        unconfirmedUser.setAccountStatus(AccountStatus.CONFIRMED);
+        unconfirmedUser.setEmail("");
+
+        post = new BlogPost();
+        post.setId(0L);
+        post.setUser(unconfirmedUser);
+        post.setEntry("");
+    }
+
+  @Test
     public void creatingNewUserShouldSetAccountStatusToNEW() {
         blogService.createUser(new UserRequest("John", "Steward", "john@domain.com"));
         ArgumentCaptor<User> userParam = ArgumentCaptor.forClass(User.class);
         Mockito.verify(userRepository).save(userParam.capture());
         User user = userParam.getValue();
         Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
+    }
+
+    @Test(expected = DomainError.class)
+    public void addLikeToPost_shouldFail_whenUserIsNotConfirmed() {
+      Mockito.when(userRepository.findById(unconfirmedUser.getId())).thenReturn(Optional.of(unconfirmedUser));
+      Mockito.when(postRepository.findById(post.getId())).thenReturn(Optional.of(post));
+
+      blogService.addLikeToPost(unconfirmedUser.getId(), post.getId());
     }
 
 }
